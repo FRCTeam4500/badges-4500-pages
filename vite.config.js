49cf8c7b9048cfa726bcf9eae05d26e6af9dec7d
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-09-06 17:54:01 -0500

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/badges-4500-pages",
-  plugins: [react()],
-})
